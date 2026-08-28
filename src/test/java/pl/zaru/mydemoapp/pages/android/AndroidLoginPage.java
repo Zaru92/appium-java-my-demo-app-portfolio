020dc39c4f5b2f37cf5c29bd41be2ff0b98ad0dc
Zaru92 <63313503+Zaru92@users.noreply.github.com>
2026-08-28 10:17:11 +0200

    test: add login form validation scenarios (#8)
    Co-authored-by: Michał Woźniak <>

--- src/test/java/pl/zaru/mydemoapp/pages/android/AndroidLoginPage.java
+++ src/test/java/pl/zaru/mydemoapp/pages/android/AndroidLoginPage.java
@@ -2,9 +2,11 @@ package pl.zaru.mydemoapp.pages.android;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import java.util.Objects;
 import org.openqa.selenium.By;
 import pl.zaru.mydemoapp.pages.base.BasePage;
 import pl.zaru.mydemoapp.pages.contracts.LoginPage;
+import pl.zaru.mydemoapp.pages.contracts.LoginValidation;
 
 public final class AndroidLoginPage extends BasePage implements LoginPage {
 
@@ -19,6 +21,11 @@ public final class AndroidLoginPage extends BasePage implements LoginPage {
 
   private static final By LOGIN_BUTTON = AppiumBy.id("com.saucelabs.mydemoapp.android:id/loginBtn");
 
+  private static final By USERNAME_REQUIRED_MESSAGE = AppiumBy.id(APP_PACKAGE + ":id/nameErrorTV");
+
+  private static final By PASSWORD_REQUIRED_MESSAGE =
+      AppiumBy.id(APP_PACKAGE + ":id/passwordErrorTV");
+
   public AndroidLoginPage(AppiumDriver driver) {
     super(driver);
   }
@@ -30,8 +37,19 @@ public final class AndroidLoginPage extends BasePage implements LoginPage {
 
   @Override
   public void login(String username, String password) {
-    replaceText(USERNAME_INPUT, username, "username");
-    replaceText(PASSWORD_INPUT, password, "password");
+    replaceTextAllowingEmpty(USERNAME_INPUT, username, "username");
+    replaceTextAllowingEmpty(PASSWORD_INPUT, password, "password");
     tap(LOGIN_BUTTON);
+  }
+
+  @Override
+  public boolean isValidationDisplayed(LoginValidation validation) {
+    By locator =
+        switch (Objects.requireNonNull(validation, "validation must not be null")) {
+          case USERNAME_REQUIRED -> USERNAME_REQUIRED_MESSAGE;
+          case PASSWORD_REQUIRED -> PASSWORD_REQUIRED_MESSAGE;
+        };
+
+    return waitUntilVisible(locator).isDisplayed();
   }
 }
